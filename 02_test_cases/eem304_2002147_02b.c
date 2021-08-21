af6c0e25d4f3f10b8c13fa6f343ba81475204352
nij mehar <nijmehar@Gurus-MacBook-Air.local>
2021-08-21 17:04:30 +0530

    assignment 02

---
+++ 02_test_cases/eem304_2002147_02b.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include <stdlib.h>
+void insertionsort(int a[],int N){ 
+     int i,key,j;
+    for(i=0;i<N;i++){
+        key = a[i];
+        j=i-1;
+    
+        while(j>=0 && a[j]>key){
+            a[j+1]=a[j];
+            j-=1;
+        }
+
+        a[j+1]=key;
+    }
+    for(i=0;i<N;i++){
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+}
+void selectionsort(int a[],int N){
+    int def;
+    int i,j,k;
+    for(j=0;j<N;j++){
+        def=a[j];
+        k=j;
+       for(i=j+1;i<N;i++){
+         if(a[i]<def){
+            def=a[i];
+            k=i;
+         }
+        }
+        a[k] = a[j];
+        a[j] = def;     
+    }
+    for(i=0;i<N;i++){
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+    
+}
+void bubblesort(int a[],int N){
+    int i,def,x=1;
+    while(x!=0)
+    {
+        x=0;
+        for(i=0;i<N-1;i++){
+        if(a[i]>a[i+1]){
+            def = a[i+1];
+            a[i+1] = a[i];
+            a[i] = def;
+            x++;
+         }
+        }
+    }
+     for(i=0;i<N;i++){
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+}
+int main(){
+    int size,i;
+    scanf("%d",&size);
+    int arr[size];
+    for(i=0;i<size;i++){
+        arr[i]=rand()%100;
+    }
+    insertionsort(arr,size);
+    selectionsort(arr,size);
+    bubblesort(arr,size);
+    return 0;
+}
